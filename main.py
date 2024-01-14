3b201395ab7f214d801ac1104a9a63ce35617240
Jeevan <jeevanhm308@gmail.com>
2024-01-14 22:01:28 +0530

    flask app

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import chatbot
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 
 @app.route("/", methods=["GET"])
